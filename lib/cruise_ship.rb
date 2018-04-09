16388962ebf6cbd6799c1a81a579e3b39480d174
Monica Marziano <mmarziano1@gmail.com>
2018-04-09 09:34:07 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,11 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  select_winner.each do |suite, name|
+    if suite == :suite && name.start_with?("A")
+      winner = name 
+    end
+  end
+  
+  
 end
